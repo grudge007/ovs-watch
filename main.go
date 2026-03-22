2cea188555cd6edcea6129502ea6bd26e83d40b1
Anirudh <tpanirudh7@gmail.com>
2026-03-22 13:33:04 +0530

    completed bridge-watch

--- main.go
+++ main.go
@@ -5,60 +5,71 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 func main() {
 	out, err := loadExistingBridges(Scanner)
-	fmt.Printf("%v\n", out)
-	// watchBridge(out, Scanner)
-
+	fmt.Println("ovs-watch initilized..")
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
+	watchBridge(out, Scanner)
 
 }
 
-func loadExistingBridges(Scanner func([]byte) []string) (map[string]bool, error) {
-	existingBridges := make(map[string]bool)
+func loadExistingBridges(Scanner func([]byte, bool) map[string]bool) (map[string]bool, error) {
 	cmd := exec.Command("ovs-vsctl", "list-br")
 	output, err := cmd.Output()
 
 	if err != nil {
 		return nil, err
 	}
-	bridges := Scanner(output)
-	for _, bridge := range bridges {
-		existingBridges[bridge] = true
-	}
+	existingBridges := Scanner(output, true)
 
 	return existingBridges, err
 }
 
-func watchBridge(loadedBridges map[string]bool, Scanner func([]byte) []string) {
-	count := len(loadedBridges)
-	// found := true
+func watchBridge(loadedBridges map[string]bool, Scanner func([]byte, bool) map[string]bool) {
 	for {
 		cmd := exec.Command("ovs-vsctl", "list-br")
-		out, _ := cmd.CombinedOutput()
-		bridges := Scanner(out)
-		if len(bridges) == count {
-			continue
+		out, _ := cmd.Output()
+		CurrentBridges := Scanner(out, true)
+
+		// check for newly added bridges
+		for bridge := range CurrentBridges {
+
+			if loadedBridges[bridge] {
+				continue
+			} else {
+				loadedBridges[bridge] = true
+				fmt.Printf("Created: %v\n", bridge)
+			}
+
 		}
-		if len(bridges) > count {
-			fmt.Printf("Created: %v\n", bridges[count])
-			count++
+
+		// check for deleted bridges
+		for bridge := range loadedBridges {
+			if CurrentBridges[bridge] {
+				continue
+			} else {
+				delete(loadedBridges, bridge)
+				fmt.Printf("Deleted: %v\n", bridge)
+
+			}
 		}
+		time.Sleep(5 * time.Second)
 
 	}
 }
 
-func Scanner(input []byte) []string {
-	var outPut []string
+func Scanner(input []byte, value bool) map[string]bool {
+	outPut := make(map[string]bool)
 	scanner := bufio.NewScanner(strings.NewReader(string(input)))
 	for scanner.Scan() {
 		line := scanner.Text()
-		outPut = append(outPut, line)
+		outPut[line] = value
 	}
 	return outPut
 }
